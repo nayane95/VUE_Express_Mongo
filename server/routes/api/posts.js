328327b27af5cec6fadf27d48e3b4a71e57af74e
Nayanajith Priyasad <nayanajithpriyasad@gmail.com>
2020-05-13 11:26:48 +0530

    first commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const mongodb=require('mongodb');
+
+const router = express.Router();
+
+//Get Posts
+router.get('/',(req,res) => {
+  res.send('hello');
+});
+
+//Add Post
+
+
+
+// Delete Post
+
+
+
+
+module.exports=router;
