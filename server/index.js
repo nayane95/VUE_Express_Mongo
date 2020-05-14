b41ebc052343d6ac94ab476981ab87374b179d76
Nayanajith Priyasad <nayanajithpriyasad@gmail.com>
2020-05-14 14:51:02 +0530

    Added static folder and prep SPA

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ const posts=require('./routes/api/posts');
 
 app.use('/api/posts',posts);
 
+//Handle Production
+if (process.env.NODE_ENV==='production') {
+    //static folder
+    app.use(express.static(__dirname+'/public/'));
+
+
+    //Handle SPA
+    app.get(/.*/,(req,res) => res.sendFile(__dirname+'/public/index.html'))
+}
+
 //in localhost run in 5000 for heroku use env.PORT
 const port=process.env.PORT || 5000;
 
